b14e420c6ec5742c82a5bc130f8c3f907cfc715e
Kym Michael <kym.michael@gmail.com>
2025-01-01 10:26:45 +0800

    Update setupVMorPC.sh

--- setupVMorPC.sh
+++ setupVMorPC.sh
@@ -1,19 +1,41 @@
 #!/usr/bin/sh
+git="https://github.com/sustance/configs/blob/main/"
+aaa="/home/aaa/"
 # For Linux(debian) OpenBSD and FreeBSD
+# Assumes user is "aaa"
+#sudo -u user
+#doas -u user
+pkg_add curl rofi micro cwm
 
-cd ~
+EOF
+
+cd /home/aaa/
+
+#no curl on Obsd 
+pkg_add curl rofi micro cwm
+curl ${git}mm ${aaa}.local/bin/mm
+curl ${git}config.rasi ${aaa}.config/rofi/config.rasi
+curl ${git}.cwmrc ${aaa}.cwmrc
+
+
+
+
 cat "exec cwm" >> .xinitrc
 #cat "Xft.dpi: 196" >> .xsession
-cat 'export PATH="$HOME/.local/bin:$PATH"' >> .bashrc
+touch .bashrc
+echo 'export PATH="$HOME/.local/bin:$PATH"' >> /home/aaa/.bashrc
 
+#no curl on Obsd 
+curl https://github.com/sustance/configs/blob/main/mm /home/aaa/.local/bin/mm
 
-curl https://github.com/sustance/configs/blob/main/mm .local/bin/
+curl https://github.com/sustance/configs/blob/main/config.rasi ~/local/bin/config.rasi
 
-curl https://github.com/sustance/configs/blob/main/config.rasi ~/local/bin/
-
-curl https://raw.githubusercontent.com/sustance/configs/refs/heads/main/.cwmrc ~
+curl https://raw.githubusercontent.com/sustance/configs/refs/heads/main/.cwmrc ~/.cwmrc
 
 echo "Now... Obsd: 'login root', Debian: sudo Fbsd: doas 
 echo "Run suggested script as root"
 echo "install rofi micro cwm
 echo " As root chmod +x ~/.local/bin/mm
+
+
+
