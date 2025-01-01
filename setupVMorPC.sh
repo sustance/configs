d34721e2f3892a58693e39ebfa3fbcb926054beb
Kym Michael <kym.michael@gmail.com>
2025-01-01 13:22:40 +0800

    Update setupVMorPC.sh

--- setupVMorPC.sh
+++ setupVMorPC.sh
@@ -6,12 +6,12 @@ curl ${git}mm /home/aaa/.local/bin/mm
 curl ${git}config.rasi /home/aaa/.config/rofi/config.rasi
 curl ${git}.cwmrc /home/aaa/.cwmrc
 
-echo "exec cwm" >> .xinitrc
-#cat "Xft.dpi: 196" >> .xsession
+echo "exec cwm" >> .xinitrc 
+cat "Xft.dpi: 196" >> .xsession
 touch .bashrc
 echo 'export PATH="$HOME/.local/bin:$PATH"' >> /home/aaa/.bashrc
 
-curl https://github.com/sustance/configs/blob/main/mm /home/aaa/.local/bin/mm
-curl https://github.com/sustance/configs/blob/main/config.rasi ~/local/bin/config.rasi
-curl https://raw.githubusercontent.com/sustance/configs/refs/heads/main/.cwmrc ~/.cwmrc
+curl https://github.com/sustance/configs/blob/main/mm -o /home/aaa/.local/bin/mm
+curl https://github.com/sustance/configs/blob/main/config.rasi -o /home/aaa/.config/rofi/config.rasi
+curl https://raw.githubusercontent.com/sustance/configs/refs/heads/main/.cwmrc -o /home/aaa/.cwmrc
 
