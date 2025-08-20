72efa52a0a01a44c99de4fc582dd31c2c8c17737
Kym Michael <kym.michael@gmail.com>
2025-08-20 19:29:29 +0800

    Update i.php

--- php/i.php
+++ php/i.php
@@ -129,12 +129,15 @@ Quick setup for basic SIP testing<br>
 Use username: test, password: test1234<br>
 Server: sip.zoiper.com (port 5060)</p>
 </div>
-  
+
+<pre>
 <?php
 $csv25 = file_get_contents('https://raw.githubusercontent.com/sustance/configs/refs/heads/main/php/IATAbp25csv.php');
 echo $csv25;
 ?>
+</pre>
 
+  
 <?php
 $tail = file_get_contents('https://raw.githubusercontent.com/sustance/sustance.github.io/refs/heads/main/tail-land.html');
 echo $tail;
