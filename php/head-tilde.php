615afd32b3549270e5c7419eb5b697de2ce2fc7b
Kym Michael <kym.michael@gmail.com>
2025-08-21 11:13:22 +0800

    Update head-tilde.php

--- php/head-tilde.php
+++ php/head-tilde.php
@@ -2,8 +2,7 @@
     <title>Pubnix</title>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Simplified Responsive Page</title>
-    <link rel="stylesheet" type="text/css" href="style.css" />
+    <title>Simple Page</title>
  <link rel="stylesheet" href="https://sustance.github.io/head.css" />
 
 
