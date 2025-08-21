a483f850eb882c8e33465e6b34acaf900a1e3f93
Kym Michael <kym.michael@gmail.com>
2025-08-21 08:44:55 +0800

    Update head-tilde.php

--- php/head-tilde.php
+++ php/head-tilde.php
@@ -5,18 +5,7 @@
     <link rel="stylesheet" type="text/css" href="style.css" />
 </head>
 
-    <div class="container">
-        <nav>
-            <ul>
-                <?php
-                $files = glob('*.{php,htm}', GLOB_BRACE);
-                foreach($files as $file) {
-                    $name = pathinfo($file, PATHINFO_FILENAME);
-                    echo "<li><a href='$file'>$name</a></li>";
-                }
-                ?>
-            </ul>
-        </nav>
+
         <div class="content">
         
             <img src="http://identity2.com/bg2.webp" alt="" height="60%" width="80%">
