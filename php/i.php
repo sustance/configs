2048460613ddd7dee36b8978007be7063395324a
Kym Michael <kym.michael@gmail.com>
2025-08-26 18:18:19 +0800

    Update i.php

--- php/i.php
+++ php/i.php
@@ -8,47 +8,55 @@ echo $head;
 <i>DE.</i> <b>b</b>sd.tilde.team <span class="sml">157.90.196.52</span> 
 <a href="https://bsd.tilde.team/">b.t.t</a> 
 <a href="https://bsd.tilde.team/~identity2">/~i52</a> 
+  b.i.c
 <br>
 
 <i>US.</i> <b>c</b>trl-c.club <span class="tin">165.227.127.54</span> 
 <a href="https://ctrl-c.club/">c.c</a>
-<a href="https://ctrl-c.club/~i52">/~i72</a>
+<a href="https://ctrl-c.club/~i52">/~i52</a>
 <a href="https://ctrl-c.club/~identity2/lineage/">/lin</a>   
+  [~]
 <i>News</i> 
 <br>
 
 <i>GB.</i> <b>d</b>imesion.sh <span class="sml">209.97.187.90</span> 
 <a href="https://dimension.sh/">d.s</a> 
-<a href="https://dimension.sh/~identity2">d.s/~i72</a> d.identity2.com
+<a href="https://dimension.sh/~identity2">d.s/~i52</a> 
+  d.i.c
 <br> 
 
 <i>DE.</i> <b>e</b>nvs.net <span class="sml">89.163.145.170</span> 
-<a href="https://mail.envs.net/webmail/#?action=listmailbox">i72@e.n</a>
+<a href="https://mail.envs.net/webmail/#?action=listmailbox">i52@e.n</a>
 <a href="https://envs.net/">e.n</a>  
 <a href="https://envs.net/~identity2">/~i72</a>  
-<i>remi</i> e.identity2.com
+<i>remi</i> 
+  e.i.c
 <br>
 
 <i>DE.</i> <b>f</b>reeshell.de <span class="sml">116.202.128.144</span> 
 <a href="https://freeshell.de/">f.d</a> 
 <a href="https://freeshell.de/~identity">/~i6y</a> 
+  [~]
 <br>
 
 <i>CA.</i>  tilde.<b>g</b>uru <span class="sml">95.179.178.246</span> 
 <a href="https://tilde.guru">t.g</a>  
 <a href="https://tilde.guru/~edi">/~edi</a>
+  [~]
 <br>
 
 <i>CA.</i>  t<b>h</b>unix.net <span class="sml">142.44.150.185</span> 
 <a href="https://thunix.net">t.n</a>  
 <a href="https://thunix.net/~id">/~id</a> 
-<a href="https://tilde.tel">t.tel</a>  h.identity2.com/~id/
+<a href="https://tilde.tel">t.tel</a>  
+  [~]
 <br>
 
 <i>DE.</i> tilde.<b>i</b>nstitute. <span class="sml">opt-svr</span>
 <span class="sml">159.69.146.152</span>  
 <a href="https://tilde.institute">t.i</a>  
-<a href="https://id2.tilde.institute">id2.t.i</a>   
+<a href="https://id2.tilde.institute">id2.t.i</a> 
+  id2.ic
 <br>
 
 <i>DE.</i> pro<b>j</b>ectsegfau.lt <span class="sml">45.145.41.226</span> 
@@ -64,6 +72,7 @@ echo $head;
 <i>CA.</i> tilde.<b>p</b>ink  
 <span class="sml">198.50.210.248 </span> <a href="https://tilde.pink/">t.p</a>  
 <a href="https://tilde.pink/~id2">t.p/~id2</a> 
+   [~]
 <br>
 
 <i>CA.</i> tilde.<b>t</b>eam <span class="sml">198.50.210.248</span>  <a href="https://tilde.team/">t.t</a> 
@@ -75,11 +84,13 @@ echo $head;
 <i>US.</i> <b>v</b>ern.cc <span class="sml">5.161.108.85</span> 
 <a href="https://vern.cc/">v.c</a> 
 <a href="https://id.vern.cc/">id.v.c/~id</a>
+  no web
 <br>
 
 <i>US</i> rawte<b>x</b>t.club <span class="sml">45.33.66.185</span> 
 <a href="https://rawtext.club/">r.c</a> 
 <a href="https://rawtext.club/~identity2">r.c/~i72</a> 
+  no web
 <br>
 
 <i>US</i> <b>github.io</b> <span class="sml">45.33.66.185</span> sustancegithub.io
