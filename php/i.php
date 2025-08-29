75ceb483ef5d08b83ec7898ecd1140d34f08561d
Kym Michael <kym.michael@gmail.com>
2025-08-29 18:16:58 +0800

    Update i.php

--- php/i.php
+++ php/i.php
@@ -15,7 +15,7 @@ echo $head;
 <a href="https://ctrl-c.club/">c.c</a>
 <a href="https://ctrl-c.club/~i52">/~i52</a>
 <a href="https://ctrl-c.club/~identity2/lineage/">/lin</a> 
-<a href="https://c.identity2.com">c.i.c[~]</a>
+<a href="https://c.identity2.com">c.i.c~</a>
 <i>News</i> 
 <br>
 
@@ -35,44 +35,45 @@ echo $head;
 <i>DE.</i> <b>f</b>reeshell.de <span class="sml">116.202.128.144</span> 
 <a href="https://freeshell.de/">f.d</a> 
 <a href="https://freeshell.de/~identity">/~i4y</a> 
-  [~] f.i.c works
+    <a href="https://f.identity2.com">f.i.c~</a>
 <br>
 
 <i>CA.</i>  tilde.<b>g</b>uru <span class="sml">95.179.178.246</span> 
 <a href="https://tilde.guru">t.g</a>  
 <a href="https://tilde.guru/~edi">/~edi</a>
-  [~] g.i.c works
+  <a href="https://g.identity2.com">g.i.c~</a>
 <br>
 
 <i>CA.</i>  t<b>h</b>unix.net <span class="sml">142.44.150.185</span> 
 <a href="https://thunix.net">t.n</a>  
 <a href="https://thunix.net/~id">/~id</a> 
 <a href="https://tilde.tel">t.tel</a>  
-  [~] no i.c set but not work
+<a href="https://h.identity2.com">h.i.c~</a>
 <br>
 
 <i>DE.</i> tilde.<b>i</b>nstitute. <span class="sml">opt-svr</span>
 <span class="sml">159.69.146.152</span>  
 <a href="https://tilde.institute">t.i</a>  
 <a href="https://id2.tilde.institute">id2.t.i</a> 
-  id2.ic
+<a href="https://i.identity2.com">i.i.c</a>
 <br>
 
 <i>DE.</i> pro<b>j</b>ectsegfau.lt <span class="sml">45.145.41.226</span> 
 <a href="https://projectsegfau.lt/">p.l</a> 
-<a href="https://identity2.p.projectsegfau.lt/">i.p.p.l/~i52</a>  
+<a href="https://identity2.p.projectsegfau.lt/">i.p.p.l/~i52</a> 
+    <a href="https://j.identity2.com">j.i.c</a>
 <br>
 
 <i>DE.</i> pr<b>o</b>jectsegfau.lt <span class="sml">45.145.41.226</span>
 <a href="https://projectsegfau.lt/">p.l</a> 
 <a href="https://id.p.projectsegfau.lt/">i.p.p.l/~id</a>
-should but no has web and i.c                                                  
+    <a href="https://o.identity2.com">o.i.c</a>                                              
 <br>
 
 <i>CA.</i> tilde.<b>p</b>ink  
 <span class="sml">198.50.210.248 </span> <a href="https://tilde.pink/">t.p</a>  
 <a href="https://tilde.pink/~id2">t.p/~id2</a> 
-   [~]
+  <a href="https://p.identity2.com">p.i.c~</a>
 <br>
 
 <i>CA.</i> tilde.<b>t</b>eam <span class="sml">198.50.210.248</span>  <a href="https://tilde.team/">t.t</a> 
@@ -84,23 +85,15 @@ should but no has web and i.c
 <i>US.</i> <b>v</b>ern.cc <span class="sml">5.161.108.85</span> 
 <a href="https://vern.cc/">v.c</a> 
 <a href="https://id.vern.cc/">id.v.c/~id</a>
-  no web/i.c
 <br>
 
 <i>US</i> rawte<b>x</b>t.club <span class="sml">45.33.66.185</span> 
 <a href="https://rawtext.club/">r.c</a> 
 <a href="https://rawtext.club/~identity2">r.c/~i52</a> 
-  no web/i.c
 <br>
 
 <i>US</i> <b>github.io</b> <span class="sml">45.33.66.185</span> sustancegithub.io
 <br>
-  
-<i>US.</i>    <b>s</b>fo     <span class="sml">104.245.33.223</span> 
-<a href="http://identity2.com">http i.c</a> 
-<a href="http://nix.identity2.com">http nix.i.c</a>
-<br>
-
 <i>HK.</i> <span class="sml">  Huawei</span> Hu<b>4</b>
 <span class="sml">sufbo.tplinkdns.com</span><br>
 
