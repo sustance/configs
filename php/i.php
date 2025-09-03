355f195a55507d619065af3048c93fdf1214c7e3
Kym Michael <kym.michael@gmail.com>
2025-09-03 18:25:25 +0800

    Update i.php

--- php/i.php
+++ php/i.php
@@ -8,21 +8,21 @@ echo $head;
 <i>DE.</i> <b>b</b>sd.tilde.team <span class="sml">157.90.196.52</span> 
 <a href="https://bsd.tilde.team/">b.t.t</a> 
 <a href="https://bsd.tilde.team/~identity2">/~i52</a> 
-<a href="https://b.identity2.com">b.i.c</a>
+<a href="http://b.identity2.com">b.i.c</a>
 <br>
 
 <i>US.</i> <b>c</b>trl-c.club <span class="tin">165.227.127.54</span> 
 <a href="https://ctrl-c.club/">c.c</a>
 <a href="https://ctrl-c.club/~identity2">/~i52</a>
 <a href="https://ctrl-c.club/~identity2/lineage/">/lin</a> 
-<a href="https://c.identity2.com">c.i.c~</a>
+<a href="http://c.identity2.com">c.i.c~</a>
 <i>News</i> 
 <br>
 
 <i>GB.</i> <b>d</b>imesion.sh <span class="sml">209.97.187.90</span> 
 <a href="https://dimension.sh/">d.s</a> 
 <a href="https://dimension.sh/~identity2">/~i42</a> 
-<a href="https://d.identity2.com">d.i.c</a>
+<a href="http://d.identity2.com">d.i.c</a>
 <br> 
 
 <i>DE.</i> <b>e</b>nvs.net <span class="sml">89.163.145.170</span> 
@@ -35,39 +35,39 @@ echo $head;
 <i>DE.</i> <b>f</b>reeshell.de <span class="sml">116.202.128.144</span> 
 <a href="https://freeshell.de/">f.d</a> 
 <a href="https://freeshell.de/~identity">/~i4y</a> 
-    <a href="https://f.identity2.com">f.i.c</a>
+    <a href="http://f.identity2.com">f.i.c</a>
 <br>
 
 <i>CA.</i>  tilde.<b>g</b>uru <span class="sml">95.179.178.246</span> 
 <a href="https://tilde.guru">t.g</a>  
 <a href="https://tilde.guru/~edi">/~edi</a>
-  <a href="https://g.identity2.com">g.i.c</a>
+<a href="http://g.identity2.com">g.i.c</a>
 <br>
 
 <i>CA.</i>  t<b>h</b>unix.net <span class="sml">142.44.150.185</span> 
 <a href="https://thunix.net">t.n</a>  
 <a href="https://thunix.net/~id">/~id</a> 
 <a href="https://tilde.tel">t.tel</a>  
-<a href="https://h.identity2.com">h.i.c</a>
+<a href="http://h.identity2.com">h.i.c</a>
 <br>
 
 <i>DE.</i> tilde.<b>i</b>nstitute. <span class="sml">opt-svr</span>
 <span class="sml">159.69.146.152</span>  
 <a href="https://tilde.institute">t.i</a>  
 <a href="https://id2.tilde.institute">id2.t.i</a> 
-<a href="https://i.identity2.com">i.i.c</a>
+<a href="http://i.identity2.com">i.i.c</a>
 <br>
 
 <i>DE.</i> pro<b>j</b>ectsegfau.lt <span class="sml">45.145.41.226</span> 
 <a href="https://projectsegfau.lt/">p.l</a> 
 <a href="https://identity2.p.projectsegfau.lt/">i.p.p.l/~i52</a> 
-    <a href="https://j.identity2.com">j.i.c</a>
+<a href="http://j.identity2.com">j.i.c</a>
 <br>
 
 <i>DE.</i> pr<b>o</b>jectsegfau.lt <span class="sml">45.145.41.226</span>
 <a href="https://projectsegfau.lt/">p.l</a> 
 <a href="https://id.p.projectsegfau.lt/">i.p.p.l/~id</a>
-    <a href="https://o.identity2.com">o.i.c</a>                                              
+<a href="http://o.identity2.com">o.i.c</a>                                              
 <br>
 
 <i>CA.</i> tilde.<b>p</b>ink  
@@ -79,9 +79,17 @@ echo $head;
 <i>CA.</i> tilde.<b>t</b>eam <span class="sml">198.50.210.248</span>  <a href="https://tilde.team/">t.t</a> 
 <a href="https://tilde.team/~identity2">/~i52</a> 
 <a href="https://tilde.team/~identity2/lineage">/lin</a> 
-<a href="https://t.identity2.com">t.i.c</a>
+<a href="http://t.identity2.com">t.i.c</a>
 <i>News</i>
 <br>
+
+  
+<i>US</i> gith<b>u</b>b.io <span class="sml">45.33.66.185</span> 
+<a href="https://sustance.github.io">s.g.io</a>
+<a href="https://identity2.com">i.c</a>
+<a href="http://identity2.com">www.i.c</a>
+<br>
+  
 
 <i>US.</i> <b>v</b>ern.cc <span class="sml">5.161.108.85</span> 
 <a href="https://vern.cc/">v.c</a> 
@@ -93,37 +101,19 @@ no web
 no web
 <br>
 
-<i>US</i> gith<b>u</b>b.io <span class="sml">45.33.66.185</span> 
-<a href="https://sustance.github.io">s.g.io</a>
-<a href="https://identity2.com">i.c</a>
-<a href="https://identity2.com">www.i.c</a>
-<br>
   
 <i>HK.</i> <span class="sml">Huawei64</span> Hu<b>4</b>
 <a href="https://sufbo.tplinkdns.com:8004">sufbo.tplinkdns.com:8004</a>
-<a href="https://4.identity2.com">4.i.c</a>
+<a href="http://4.identity2.com">4.i.c</a>
 <br>
 
   
 <i>HK.</i> <span class="sml">HPMini32</span> Mi<b>7</b>
 <a href="https://sufbo.tplinkdns.com:8007">sufbo.tplinkdns.com:8007</a>
-<a href="https://7.identity2.com">7.i.c</a>
+<a href="http://7.identity2.com">7.i.c</a>
 </p> 
 </div>
 
-<div class="box"> 
-<pre>
-e  111 112  60   1 112  60 333 112  60 333 112     
-d  333 112  60 333 112  60 333 112  60 333 112    
-t  264 297 274 264 297 274 264 297 274 264 333 
-e  111 112  60   1 112  60 333 112  60 333 112 
-</pre>
-<p>
-e 1111 112 60 1 112 60 333 112 60 333 112<br>      
-d 333 112 60 333 112 60 333 112 60 333 112<br>      
-t 264 297 274 264 297 274 264 297 274 264<br>      
-</p>                                                                     
-</div>
 
 <div class="box"> 
 <p><i>Mob</i> <b>le</b><span class="sml">novo</p>
