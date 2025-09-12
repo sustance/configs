0ab60be49c4117f2e1696986d70c41bab95f274d
Kym Michael <kym.michael@gmail.com>
2025-09-12 19:06:45 +0800

    Update i.php

--- php/i.php
+++ php/i.php
@@ -30,6 +30,24 @@ echo $svg_icon_content;
   src="https://github.com/sustance.github.io/assets/globe.svg"
   alt="Home" height="32" width="32" />
 
+<svg width="100%" height="100%">
+  <rect width="100%" height="100%" fill="red" />
+  <circle cx="100%" cy="100%" r="150" fill="blue" stroke="black" />
+  <polygon points="120,0 240,225 0,225" fill="green" />
+  <text
+    x="50"
+    y="100"
+    font-family="Verdana"
+    font-size="55"
+    fill="white"
+    stroke="black"
+    stroke-width="2">
+    Hello!
+  </text>
+</svg>
+
+
+
 <div class="bsd">
 <p>
 <i>DE.</i> <b>b</b>sd.tilde.team <span class="sml">157.90.196.52</span> 
