533ea64be08de9c4d28a0b388698554b3924ea2b
Kym Michael <kym.michael@gmail.com>
2025-09-12 19:13:34 +0800

    Update i.php

--- php/i.php
+++ php/i.php
@@ -2,7 +2,7 @@
 $head = file_get_contents('https://raw.githubusercontent.com/sustance/configs/refs/heads/main/php/head-land.html');
 echo $head;
 ?>
-
+<div class="bsd">
 <img
   src="https://sustance.github.io/assets/home.svg"
   alt="Home" height="64" width="64" />
@@ -10,22 +10,22 @@ echo $head;
   src="https://sustance.github.io/assets/globe.svg"
   alt="Home" height="32" width="32" />
 
-<svg width="100%" height="100%">
-  <rect width="100%" height="100%" fill="red" />
+<svg width="88%" height="100%">
+  <rect width="88%" height="100%" fill="red" />
   <circle cx="100%" cy="100%" r="150" fill="blue" stroke="black" />
   <polygon points="120,0 240,225 0,225" fill="green" />
   <text
     x="50"
     y="100"
     font-family="Verdana"
-    font-size="55"
+    font-size="75"
     fill="white"
     stroke="black"
-    stroke-width="2">
-    Hello!
+    stroke-width="1">
+    Identity2
   </text>
 </svg>
-
+</div>
 
 
 <div class="bsd">
