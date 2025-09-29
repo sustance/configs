b4b8ad29075a79c309b52f046997a2f09385d910
Kym Michael <kym.michael@gmail.com>
2025-09-29 08:34:43 +0800

    Update i.php

--- php/i.php
+++ php/i.php
@@ -17,7 +17,7 @@ echo $head;
   
 <div class="bsd">
 <p>
-  CA.
+CA.
 <a href="https://bsd.tilde.team/"> <b>b</b>sd.tilde.team </a> 
 <a href="https://bsd.tilde.team/~identity2">/~i52</a> 
 <a href="http://b.identity2.com">b.i.c</a>
@@ -35,10 +35,7 @@ GB.
 <a href="https://dimension.sh/~identity2">/~i42</a> 
 <a href="http://d.identity2.com">d.i.c</a>
   <span class="sml">209.97.187.90</span> 
-
-  
 <br> 
-
 DE.  
 <a href="https://envs.net/"><b>e</b>nvs.net</a>  
 <a href="https://envs.net/~identity2">/~i52</a>  
@@ -46,21 +43,18 @@ DE.
   <span class="sml">89.163.145.170</span>
 <i>remi</i> 
 <br>
-  
 DE. 
 <a href="https://freeshell.de/"><b>f</b>reeshell.de</a> 
 <a href="https://freeshell.de/~identity">/~i4y</a> 
     <a href="http://f.identity2.com">f.i.c</a> 
   <span class="sml">116.202.128.144</span> 
 <br>
-
 CA.  
 <a href="https://tilde.guru">tilde.<b>g</b>uru </a>  
 <a href="https://tilde.guru/~edi">/~edi</a>
 <a href="http://g.identity2.com">g.i.c</a> 
   <span class="sml">95.179.178.246</span>
 <br>
-
 CA.  
 <a href="https://thunix.net">t<b>h</b>unix.net</a>  
 <a href="https://thunix.net/~id">/~id</a> 
@@ -68,36 +62,30 @@ CA.
 <a href="http://h.identity2.com">h.i.c</a> 
   <span class="sml">142.44.150.185</span
 <br>
-
 DE. 
- 
 <a href="https://tilde.institute">tilde.<b>i</b>nstitute</a>  
 <a href="https://id2.tilde.institute">id2.t.i</a> 
 <a href="http://i.identity2.com">i.i.c</a>
   <span class="sml">159.69.146.152</span>  
 <br>
-
 DE. 
-<a href="https://projectsegfau.lt/">pro<b>j</b>ectsegfau.lt </a> 
+<a href="https://projectsegfau.lt/">i72.pro<b>j</b>ectsegfau.lt </a> 
 <a href="https://identity2.p.projectsegfau.lt/">i.p.p.l/~i52</a> 
 <a href="http://j.identity2.com">j.i.c</a> 
   <span class="sml">45.145.41.226</span>
 <br>
-
 DE. 
-<a href="https://projectsegfau.lt/">pr<b>o</b>jectsegfau.lt</a> 
+<a href="https://projectsegfau.lt/">i.7.pr<b>o</b>jectsegfau.lt</a> 
 <a href="https://id.p.projectsegfau.lt/">i.p.p.l/~id</a>
 <a href="http://o.identity2.com">o.i.c</a> 
   <span class="sml">45.145.41.226</span>                                              
 <br>
-
 CA. 
 <a href="https://tilde.pink/">tilde.<b>p</b>ink</a>  
 <a href="https://tilde.pink/~id2">t.p/~id2</a> 
   <a href="https://p.identity2.com">p.i.c</a>
   <span class="sml">198.50.210.248 </span> 
 <br>
-
 CA. 
   <a href="https://tilde.team/">tilde.<b>t</b>eam</a> 
 <a href="https://tilde.team/~identity2">/~i52</a> 
@@ -105,38 +93,30 @@ CA.
 <a href="http://t.identity2.com">t.i.c</a> 
   <span class="sml">198.50.210.248</span> 
 <br>
-
-  
 US 
 <a href="https://sustance.github.io">sustance.gith<b>u</b>b.io</a>
 <a href="https://identity2.com">i.c</a>
 <a href="http://identity2.com">www.i.c</a> 
   <span class="sml">45.33.66.185</span
 <br>
-  
-
 US. <b>v</b>ern.cc 
 <a href="https://vern.cc/">v.c</a> <span class="sml">5.161.108.85</span> 
 no web
 <br>
-
 US. rawte<b>x</b>t.club 
 <a href="https://rawtext.club/">r.c</a>  <span class="sml">45.33.66.185</span>
 no web
 <br>
-
-  
 HK.
 <a href="https://sufbo.tplinkdns.com:8004">sufbo.tplinkdns.com:8004</a>
 <a href="http://4.identity2.com">4.i.c</a>
   <span class="sml">Huawei64</span> Hu<b>4</b>
 <br>
-
-  
 HK. 
 <a href="https://sufbo.tplinkdns.com:8007">sufbo.tplinkdns.com:8007</a>
 <a href="http://7.identity2.com">7.i.c</a>
   <span class="sml">HPMini32</span> Mi<b>7</b>
+
 </p> 
 </div>
 
