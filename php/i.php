07116b1415879574f2d8c68a3791347cc09fe795
Kym Michael <kym.michael@gmail.com>
2025-10-27 09:30:12 +0800

    Create i.php

---
+++ php/i.php
@@ -0,0 +1,382 @@
+<?php 
+$head = file_get_contents('https://raw.githubusercontent.com/sustance/configs/refs/heads/main/php/head-land.html');
+echo $head;
+?>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+<!--<div class="box">
+  <p> Inline in 
+<img
+  src="https://sustance.github.io/assets/globe.svg"
+  alt="Home" height="24" width="24" />
+    text</p> 
+<img
+  src="https://sustance.github.io/assets/globe.svg"
+  alt="Home" height="64" width="64" />
+</div>
+-->
+  
+<div class="bsd">
+<p>
+HK. 
+Router<b>1</b> smb://tp-share/g/
+<br>  
+US.
+<a href="https://bsd.tilde.team/"> <b>b</b>sd.tilde.team </a> 
+<a href="https://bsd.tilde.team/~identity2">/~i52</a> 
+<a href="http://b.identity2.com">b.i.c</a>
+<span class="sml">157.90.196.52</span> 
+<br>
+<a href="https://bsd.tilde.team/~identity2/status_data.html"><span class="pr">more...py</span></a> 
+<br>
+US. 
+<a href="https://ctrl-c.club/"><b>c</b>trl-c.club</a>
+<a href="https://ctrl-c.club/~identity2">/~i52</a>
+<a href="https://ctrl-c.club/~identity2/lineage/">/lin</a> 
+<a href="http://c.identity2.com">c.i.c~</a>
+<span class="sml">165.227.127.54</span> 
+  <br>
+<a href="https://ctrl-c.club/~identity2/status_slave.php">more...</a>
+<a href="https://ctrl-c.club/~identity2/MATURrem.php">MATURrem</a> 
+<br>
+CA.
+<a href="https://dimension.sh/"><b>d</b>imension.sh </a> 
+<a href="https://dimension.sh/~identity2">/~i42</a> 
+<a href="http://d.identity2.com">d.i.c</a>
+<span class="sml">51.79.51.59</span> <br>
+<a href="https://dimension.sh/~identity2/status_slave.php">more...</a>
+DOWN, not clean cron
+<br> 
+DE.  
+<a href="https://envs.net/"><b>e</b>nvs.net</a>  
+<a href="https://envs.net/~identity2">/~i52</a>  
+<a href="https://e.identity2.com">e.i.c</a> 
+<span class="sml">89.163.145.170</span>
+<br>
+<a href="https://envs.net/~identity2/status_slave.php">more...<a>
+<i>remi</i>  <a href="https://envs.net/~identity2/iata25.php">iata25</a>  
+  <a href="https://envs.net/~identity2/points25.php">points25</a>  
+  <i>Jekyll</i> 
+<br>
+DE. 
+<a href="https://freeshell.de/"><b>f</b>reeshell.de</a> 
+<a href="https://freeshell.de/~identity">/~i4y</a> 
+<a href="http://f.identity2.com">f.i.c</a> 
+<a href="https://freeshell.de/shell/">Shell</a> 
+  <span class="sml">116.202.128.144</span> <br>
+  <a href="https://freeshell.de/~identity/status_slave.php">more...</a>
+<br>
+CA.  
+<a href="https://tilde.guru">tilde.<b>g</b>uru </a>  
+<a href="https://tilde.guru/~edi">/~edi</a>
+<a href="http://g.identity2.com">g.i.c</a> 
+<span class="sml">95.179.178.246</span><br>
+<a href="https://tilde.guru/~edi/status_data.html"><span class="pr">more...py</span></a> 
+<br>
+CA.  
+<a href="https://thunix.net">t<b>h</b>unix.net</a>  
+<a href="https://thunix.net/~id">/~id</a> 
+<a href="https://tilde.tel">t.tel</a>  
+<a href="http://h.identity2.com">h.i.c</a> 
+  <span class="sml">142.44.150.185</span><br>
+  <a href="https://thunix.net/~id/status_slave.php">more...</a>
+  <a href="https://thunix.net/~id/status_server_table.php">ALL SERVERS</a>
+  <a href="https://thunix.net/~id/diy.php">DIY</a> 
+  <a href="https://thunix.net/~id/iata25.php">iata25</a> 
+  <a href="https://thunix.net/~id/points25.php">points25</a>
+<br>
+  
+DE. 
+<a href="https://tilde.institute">tilde.<b>i</b>nstitute</a>  
+<a href="https://id2.tilde.institute">id2.t.i</a> 
+<a href="http://i.identity2.com">i.i.c</a>
+  <span class="sml">159.69.146.152</span><br>
+  <a href="https://id2.tilde.institute/status_slave.php">more...</a>  
+<br>
+DE. 
+<a href="https://projectsegfau.lt/">i72.pro<b>j</b>ectsegfau.lt </a> 
+<a href="https://identity2.p.projectsegfau.lt/">i.p.p.l/~i52</a> 
+<a href="http://j.identity2.com">j.i.c</a> 
+  <span class="sml">45.145.41.226</span><br>
+  <a href="https://identity2.p.projectsegfau.lt/status_slave.html">more...</a> PHP INSTALLED, NOT FOR WEB
+<br>
+DE. 
+<a href="https://projectsegfau.lt/">i.7.pr<b>o</b>jectsegfau.lt</a> 
+<a href="https://id.p.projectsegfau.lt/">i.p.p.l/~id</a>
+<a href="http://o.identity2.com">o.i.c</a> 
+  <span class="sml">45.145.41.226</span> <br>
+  <a href="https://id.p.projectsegfau.lt/status_slave.html">more...</a> PHP INSTALLED, NOT FOR WEB                                            
+<br>
+CA. 
+<a href="https://tilde.pink/">tilde.<b>p</b>ink</a>  
+<a href="https://tilde.pink/~id2">t.p/~id2</a> 
+  <a href="https://p.identity2.com">p.i.c</a>
+  <span class="sml">198.50.210.248 </span><br>
+  <a href="https://tilde.pink/~id2/status_slave.php">more...</a> 
+  <br>
+  <i>PHP NOT WEB</i>
+
+<br>
+CA. 
+  <a href="https://tilde.team/">tilde.<b>t</b>eam</a> 
+<a href="https://tilde.team/~identity2">/~i52</a> 
+<a href="http://t.identity2.com">t.i.c</a> 
+<span class="sml">198.50.210.248</span> <br>
+  <a href="https://tilde.team/~identity2/status_slave.php" target="_blank">more...</a> 
+  <i>Ancestor</i>
+<a href="https://tilde.team/~identity2/lineage">/Lineage</a>   
+<a href="https://tilde.team/~identity2/diy">/diy</a> 
+<a href="https://tilde.team/~identity2/points25">/points25</a> 
+<a href="https://tilde.team/~identity2/iata25">/iata25</a>  
+<br>
+US 
+<a href="https://sustance.github.io">sustance.gith<b>u</b>b.io</a>
+<a href="https://identity2.com">i.c</a>
+<a href="http://identity2.com">www.i.c</a> 
+  <span class="sml">45.33.66.185</span>
+<br>
+US. <b>v</b>ern.cc 
+<a href="https://vern.cc/">v.c</a> <span class="sml">5.161.108.85</span>
+no web
+<br>
+US. rawte<b>x</b>t.club 
+<a href="https://rawtext.club/">r.c</a>  <span class="sml">45.33.66.185</span>
+no web
+<br>
+HK. 
+Hu<b>4</b> 64b 
+<a href="https://sufbo.tplinkdns.com:8004">sufbo.tplinkdns.com:8004</a>
+<a href="http://4.identity2.com">4.i.c</a>
+<br>
+ . . . SSH: 8080yN 8443yN 443yY Router <s>443</s> > 9443
+<br>
+HK. 
+Mi<b>7</b> 32b
+<a href="https://sufbo.tplinkdns.com:8007">sufbo.tplinkdns.com:8007</a>
+<a href="http://7.identity2.com">7.i.c</a>
+<br> . . . SSH: 8888yN 8081yN
+
+</p> 
+</div>
+
+<div class="box"> 
+<p>
+Mob <b>le</b><span class="sml">novo</p>
+<p>SIP Address	2233589729@sip2sip.info<br>
+Username	2233589729  PASS: ;;6sip<br>
+Domain	sip2sip.info<br>
+Other SIP servers available To locate SIP device must lookup DNS<br>
+<br>
+Acct settings, login using credentials http://x.sip2sip.info<br>
+FAIL sufbo, k.m@g.c<br>
+<br>
+Zoiper Test Server<br>
+Domain: zoiper.com<br>
+Preconfigured test account for Zoiper softphone<br>
+<br>
+Quick setup for basic SIP testing<br>
+Use username: test, password: test1234<br>
+Server: sip.zoiper.com (port 5060)</p>
+</div>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+<?php 
+// Fetch and decode the JSON data
+$json_url = 'https://thunix.net/~id/status_server_master.json';
+$json_data = file_get_contents($json_url);
+$data = json_decode($json_data, true);
+
+// Extract unique values for menu
+$countries = [];
+$software_keys = [];
+
+if (isset($data['servers'])) {
+    foreach ($data['servers'] as $server_id => $server_info) {
+        // Get country
+        if (isset($server_info['country'])) {
+            $countries[$server_info['country']] = true;
+        }
+        
+        // Get software keys (from data->software)
+        if (isset($server_info['data']['software'])) {
+            foreach ($server_info['data']['software'] as $key => $value) {
+                $software_keys[$key] = true;
+            }
+        }
+    }
+}
+
+// Sort the arrays
+ksort($countries);
+ksort($software_keys);
+?>
+
+<div class="server-filter-container">
+    <h2>Server Filter Menu</h2>
+    
+    <div class="filter-menu">
+        <h3>Filter by Country:</h3>
+        <div class="menu-items">
+            <?php foreach (array_keys($countries) as $country): ?>
+                <a href="#" class="filter-link" data-type="country" data-value="<?php echo htmlspecialchars($country); ?>">
+                    <?php echo htmlspecialchars($country); ?>
+                </a>
+            <?php endforeach; ?>
+        </div>
+        
+        <h3>Filter by Software:</h3>
+        <div class="menu-items">
+            <?php foreach (array_keys($software_keys) as $software): ?>
+                <a href="#" class="filter-link" data-type="software" data-value="<?php echo htmlspecialchars($software); ?>">
+                    <?php echo htmlspecialchars($software); ?>
+                </a>
+            <?php endforeach; ?>
+        </div>
+    </div>
+    
+    <div id="results" class="results-container" style="display: none;">
+        <h3 id="results-title"></h3>
+        <table id="results-table">
+            <thead>
+                <tr>
+                    <th>Server ID</th>
+                    <th>Country</th>
+                    <th>Software Key</th>
+                    <th>Value</th>
+                </tr>
+            </thead>
+            <tbody id="results-body">
+            </tbody>
+        </table>
+    </div>
+</div>
+
+<script>
+// Store the JSON data in JavaScript
+const serverData = <?php echo json_encode($data); ?>;
+
+// Add click handlers to all filter links
+document.querySelectorAll('.filter-link').forEach(link => {
+    link.addEventListener('click', function(e) {
+        e.preventDefault();
+        
+        const filterType = this.dataset.type;
+        const filterValue = this.dataset.value;
+        
+        filterServers(filterType, filterValue);
+    });
+});
+
+function filterServers(filterType, filterValue) {
+    const resultsDiv = document.getElementById('results');
+    const resultsTitle = document.getElementById('results-title');
+    const resultsBody = document.getElementById('results-body');
+    
+    // Clear previous results
+    resultsBody.innerHTML = '';
+    
+    // Filter and display results
+    let rowCount = 0;
+    
+    if (serverData.servers) {
+        for (const [serverId, serverInfo] of Object.entries(serverData.servers)) {
+            // Skip if no valid data
+            if (serverInfo.status !== 'success' || !serverInfo.data || !serverInfo.data.software) {
+                continue;
+            }
+            
+            if (filterType === 'country') {
+                // Filter by country
+                if (serverInfo.country === filterValue) {
+                    // Add all software entries for this server
+                    for (const [softKey, softValue] of Object.entries(serverInfo.data.software)) {
+                        addResultRow(resultsBody, serverId, serverInfo.country, softKey, softValue);
+                        rowCount++;
+                    }
+                }
+            } else if (filterType === 'software') {
+                // Filter by software key
+                if (serverInfo.data.software[filterValue]) {
+                    addResultRow(resultsBody, serverId, serverInfo.country, filterValue, serverInfo.data.software[filterValue]);
+                    rowCount++;
+                }
+            }
+        }
+    }
+    
+    // Update title and show results
+    resultsTitle.textContent = `Results for ${filterType}: ${filterValue} (${rowCount} entries)`;
+    resultsDiv.style.display = 'block';
+    
+    // Scroll to results
+    resultsDiv.scrollIntoView({ behavior: 'smooth', block: 'nearest' });
+}
+
+function addResultRow(tbody, serverId, country, softKey, softValue) {
+    const row = document.createElement('tr');
+    row.innerHTML = `
+        <td>${escapeHtml(serverId)}</td>
+        <td>${escapeHtml(country)}</td>
+        <td>${escapeHtml(softKey)}</td>
+        <td>${escapeHtml(softValue)}</td>
+    `;
+    tbody.appendChild(row);
+}
+
+function escapeHtml(text) {
+    const div = document.createElement('div');
+    div.textContent = text;
+    return div.innerHTML;
+}
+</script>
+
+<?php
+$tail = file_get_contents('https://raw.githubusercontent.com/sustance/sustance.github.io/refs/heads/main/tail-land.html');
+echo $tail;
+?>
