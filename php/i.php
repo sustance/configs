bfdf6e1f32c3660139e92270c406c3ab2ba0c875
Kym Michael <kym.michael@gmail.com>
2025-11-06 10:14:29 +0800

    Update i.php

--- php/i.php
+++ php/i.php
@@ -42,14 +42,12 @@ foreach ($data['servers'] as $server) {
     $osClass = $server['os'] ?? 'Linux';
     echo "<p>";    
     // Basic server info
-    echo "<span class='$osClass' svrname>{$server['name']}</span> &nbsp; ";    
+    echo "<span class='$osClass' svrname>{$server['name']}</span>&nbsp; ";    
     echo "<span class='sml'>{$server['country']}</span> ";
     echo "<span class='tooltip' data-tooltip=\"IP:{$server['ip_address']} OS:{$server['os']}\">❔ </span>";
-  //echo "<span class=\"$osClass\">{$server['os']}</span>  "; 
     echo "<a href=\"http://{$server['host_url']}\">{$server['host_url']}</a> ";    
     echo "<a href=\"http://{$server['url_own']}\">i.c.{$server['name']}</a> ";    
     echo "<a href=\"http://{$server['url']}\">{$server['acc_name_s']}</a> ";    
-    echo "<span class='tooltip' data-tooltip='IP:{$server['ip_address']}'>❔</span>";
     // Links from links_http array
     if (isset($server['links']) && is_array($server['links'])) {
         foreach ($server['links'] as $link) {
