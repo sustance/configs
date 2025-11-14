2b2388d863e0893f4e7ef3aadd107957294d68c5
Kym Michael <kym.michael@gmail.com>
2025-11-14 08:54:54 +0800

    Update i.php

--- php/i.php
+++ php/i.php
@@ -48,9 +48,7 @@ foreach ($data['servers'] as $server) {
 }
 
 echo "</div >";
-?>
-https://www.google.com/search?q=external+keyboard+for+android+shortcut+keys&oq=external+keyboard+for+android+shortcut+keys&gs_lcrp=EgZjaHJvbWUyBggAEEUYOTIICAEQABgWGB4yDQgCEAAYhgMYgAQYigUyDQgDEAAYhgMYgAQYigUyCggEEAAYgAQYogQyCggFEAAYgAQYogQyBwgGECEYjwLSAQkyNzU2MmowajmoAg6wAgHxBd1VIIJKFarH8QXdVSCCShWqxw&client=ms-android-samsung-ss&sourceid=chrome-mobile&ie=UTF-8
-<?php 
+
 // Fetch and decode the JSON data
 $json_url = 'https://thunix.net/~id/status_server_master.json';
 $json_data = file_get_contents($json_url);
