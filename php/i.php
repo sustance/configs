209e539775e36c0022249a76d8df48a901c1b3dd
Kym Michael <kym.michael@gmail.com>
2025-11-14 08:48:14 +0800

    Update i.php
    concantonated php blocks

--- php/i.php
+++ php/i.php
@@ -1,26 +1,24 @@
 <?php
+
+// Fetch head section data
 $head = file_get_contents('https://raw.githubusercontent.com/sustance/configs/refs/heads/main/php/head-land.html');
 echo $head;
-?>
-<?php
 
 // Fetch JSON data
+
 $jsonUrl ';
 $jsonData = file_get_contents($jsonUrl);
 $data = json_decode($jsonData, true);
 
 // Sort servers by name
+
 usort($data['servers'], function($a, $b) {
     return strcmp($a['name'], $b['name']);
 });
 
-?>
+// Output server list
 
-<?php
 echo "<div class='box idx'>";
-
-
-
 // Output each server
 foreach ($data['servers'] as $server) {
     $osClass = $server['os'] ?? 'Linux';
@@ -119,74 +117,21 @@ ksort($software_keys);
     </div>
 </div>
 
-
-<pre>
-C
+<pre>    C E F O J T
 allow_url_fopen          : ENABLED
 allow_url_include        : DISABLED
 curl_extension           : AVAILABLE
 write_permissions        : WRITABLE
-ALL_DYNAMIC_METHODS_FAIL - Use cron fallback
-
-E
-allow_url_fopen          : ENABLED
-allow_url_include        : DISABLED
-curl_extension           : AVAILABLE
-write_permissions        : WRITABLE
-ALL_DYNAMIC_METHODS_FAIL - Use cron fallback
-
-F
-allow_url_fopen          : ENABLED
-allow_url_include        : DISABLED
-curl_extension           : AVAILABLE
-write_permissions        : WRITABLE
-ALL_DYNAMIC_METHODS_FAIL - Use cron fallback
-
-G
-NO PHP
-
-H
-PHP Fatal error:  Uncaught Error: Call to undefined function curl_exec() in /home/id/eval.php:114
-Stack trace:
-#0 /home/id/eval.php(42): DynamicExecutionTester->testCurlDirect()
-#1 /home/id/eval.php(34): DynamicExecutionTester->testMethod()
-#2 /home/id/eval.php(278): DynamicExecutionTester->testAllMethods()
-#3 {main}
-  thrown in /home/id/eval.php on line 114
-
- I
+ALL_DYNAMIC_METHODS_FAIL - Use cron fallback 
+    G    NO PHP
+    H    PHP Fatal error: 
+    I P
 allow_url_fopen          : DISABLED
 allow_url_include        : DISABLED
 curl_extension           : MISSING
 write_permissions        : WRITABLE
 ALL_DYNAMIC_METHODS_FAIL - Use cron fallback
-
-O J
-allow_url_fopen          : ENABLED
-allow_url_include        : DISABLED
-curl_extension           : AVAILABLE
-write_permissions        : WRITABLE
-ALL_DYNAMIC_METHODS_FAIL - Use cron fallback
-
-
-P
-allow_url_fopen          : ENABLED
-allow_url_include        : DISABLED
-curl_extension           : MISSING
-write_permissions        : WRITABLE
-ALL_DYNAMIC_METHODS_FAIL - Use cron fallback
-
-T
-allow_url_fopen          : ENABLED
-allow_url_include        : DISABLED
-curl_extension           : AVAILABLE
-write_permissions        : WRITABLE
-ALL_DYNAMIC_METHODS_FAIL - Use cron fallback
-
 </pre>
-
-
-
 
 <?php
 $tail = file_get_contents('https://raw.githubusercontent.com/sustance/sustance.github.io/refs/heads/main/tail-land.html');
