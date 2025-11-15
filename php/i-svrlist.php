abfa4e891e73a1b839171213f79ba45b981d034e
Kym Michael <kym.michael@gmail.com>
2025-11-15 19:52:19 +0800

    Update i-svrlist.php

--- php/i-svrlist.php
+++ php/i-svrlist.php
@@ -3,12 +3,11 @@
 $jsonUrl = 'https://raw.githubusercontent.com/sustance/configs/refs/heads/main/status_servers.json';
 $jsonData = file_get_contents($jsonUrl);
 $data = json_decode($jsonData, true);
-
 // Sort servers by name
 usort($data['servers'], function($a, $b) {
     return strcmp($a['name'], $b['name']);
 });
-//////////////////////
+// Process List
 echo "<div class='box idx'>";
 // Output each server
 foreach ($data['servers'] as $server) {
@@ -20,8 +19,7 @@ foreach ($data['servers'] as $server) {
     echo "<a href=\"http://{$server['host_url']}\">{$server['host_url']}</a> ";    
     echo "<a href=\"http://{$server['url_own']}\">i.c.{$server['name']}</a> ";    
     echo "<a href=\"http://{$server['url']}\">{$server['acc_name_s']}</a> ";    
-    echo "{$server['ip_address']} ";
-    
+    echo "{$server['ip_address']} ";   
     // Links from links_http array
     if (isset($server['links']) && is_array($server['links'])) {
         foreach ($server['links'] as $link) {
@@ -29,9 +27,7 @@ foreach ($data['servers'] as $server) {
             echo "<a href=\"http://$linkUrl\">$link</a> ";
         }
     }
-
     echo "<span>{$server['apps_running']}</span> ";
-    
     echo "</p>\n</div>";
 }
 ?>
