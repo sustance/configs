fbbcf7750bf6288b5911560760813b5a0cf71e36
Kym Michael <kym.michael@gmail.com>
2025-11-17 10:27:22 +0800

    Create i-color.php

---
+++ php/i-color.php
@@ -0,0 +1,13 @@
+<?php
+$head = file_get_contents('https://raw.githubusercontent.com/sustance/configs/refs/heads/main/php/head-land.html');
+echo $head;
+?>
+<?php
+$tail = file_get_contents('https://raw.githubusercontent.com/sustance/sustance.github.io/refs/heads/main/icolor.html');
+echo $tail;
+?>
+
+<?php
+$tail = file_get_contents('https://raw.githubusercontent.com/sustance/sustance.github.io/refs/heads/main/tail-land.html');
+echo $tail;
+?>
