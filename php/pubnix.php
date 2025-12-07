cec44c4bc808c090273b6bb68d22c451a54a6492
Kym Michael <kym.michael@gmail.com>
2025-12-07 20:02:09 +0800

    Add tilde server details to pubnix.php
    Added information about various tilde servers including their features and sign-up notes.

--- php/pubnix.php
+++ php/pubnix.php
@@ -1,1 +1,10 @@
 
+Server Name,OS,Access Method,Key Community Features,Sign-Up Notes,Website
+tilde.town,Linux,SSH (ssh tilde.town),"IRC chats, shared wiki, UUCP email, collaborative projects; focus on inclusive *nix learning and social Unix culture.",Self-signup via web form; immediate access.,tilde.town
+tilde.team,Linux,SSH (ssh tilde.team),"Educational workshops, IRC, blogs, microblogging; emphasizes teaching Unix/programming in a non-commercial space.",Web registration; quick approval.,tilde.team
+tilde.institute,OpenBSD,SSH (ssh tilde.institute),"OpenBSD experimentation, IRC, gopher/web space, games; social focus on OS education and multi-user interaction.",Signup via website; free and open.,tilde.institute
+cosmic.voyage,Linux (Debian),SSH (ssh cosmic.voyage),"Collaborative sci-fi storytelling, IRC, forums, email; community-driven narrative and creative projects.",Web form signup; part of Tildeverse.,cosmic.voyage
+aaang.net,Linux,SSH (ssh aaang.net),"IRC, gopher, games, shared web space; casual social hub for *nix enthusiasts.",Self-registration; immediate.,aaang.net
+squiggle.city,Linux,SSH (ssh squiggle.city),"Experimental projects, IRC, blogging; loose community around creative Unix use.",Web signup; free access.,squiggle.city
+tilde.pw,Linux,SSH (ssh tilde.pw),"IRC, wiki, email; focus on privacy and small-group collaboration.",Registration form; quick.,tilde.pw
+hackhub.org,Linux,SSH (ssh hackhub.org),"Hacking workshops, IRC, CTF-style games; community for security and coding learners.",Web signup; community-vetted.,hackhub.org
