790e2eaf4f88b5039c89665103f83ca7b99c8188
Kym Michael <kym.michael@gmail.com>
2025-12-31 12:38:03 +0800

    Create aiPrompts.php

---
+++ php/aiPrompts.php
@@ -0,0 +1,16 @@
+
+
+<h1>Prompts to analse iata2x.php files</h1>
+
+Process the included csv file.
+Take account of the "Notes" at the bottom
+
+Create a file to import into "My Maps" on Google maps in a new layer
+format in the most versatile format (CSV, KML, KMZ)
+
+That file should contain:
+For each trip, defined as starting from "Home base" to return to "home base".
+- place a point on map and "port name" code label for each destination.
+- join each point of each "trip" with a solid line for flights and dotted line for trains.
+- label each leg of each "trip" with the date in "MM/DD" format.
+- attempt to avoid labels overlaying one another
